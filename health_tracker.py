b8e7ad059f7f2b201360734b0350db651dd36978
CyclingNinja <bennett.sm89@gmail.com>
2017-10-19 23:27:58 +0000

    Add rest framework

--- health_tracker.py
+++ health_tracker.py
@@ -1,8 +1,10 @@
 import numpy as np
 
 class HitPoints():
-    def __init__(self, hp):
-        self.hp = hp
+    def __init__(self, total_hp):
+        self.hp = total_hp
+        self.total_hp = total_hp
+
 
     def take_pain(self, points):
         self.hp = self.hp - points
@@ -10,4 +12,8 @@ class HitPoints():
     def heal_up(self, points):
         self.hp = self.hp + points
 
-    
+    def short_rest(self):
+        pass
+
+    def long_rest(self):
+        pass
