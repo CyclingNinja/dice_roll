fd8eacc75f5626f7ae7224101e922a37d62aaf6b
CyclingNinja <bennett.sm89@gmail.com>
2017-10-21 23:03:20 +0000

    Add rests and max min checks

--- health_tracker.py
+++ health_tracker.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class HitPoints():
     def __init__(self, max_hp):
         # initialise the varying hp
@@ -10,19 +11,27 @@ class HitPoints():
         self.bloodied = max_hp / 2.0
         self.death = self.bloodied * -1.0
 
-
     def take_pain(self, points):
         self.hp = self.hp - points
 
+        if self.hp < self.bloodied:
+            return "You are bloodied"
+        elif self.hp <= 0:
+            return "Curl up and await the icy embrace of death"
+        else:
+            pass
 
     def heal_up(self, points):
         self.hp = self.hp + points
 
+        if self.hp > self.max_hp:
+            self.hp = self.max_hp
+
     def short_rest(self):
-        pass
+        self.hp = self.hp + self.max_hp / 2.0
 
     def long_rest(self):
-        pass
+        self.hp = self.max_hp
 
     def get_points(self):
         print("Current Points : {}".format(self.hp))
